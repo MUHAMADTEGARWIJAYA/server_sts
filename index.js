0799a0add438cf439b40cbd62d94103963f1ac3a
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2025-06-14 16:19:51 +0700

    modifikasi vercel

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+));
 
 connectDB();
 
